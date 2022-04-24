84730bb8ef205493020adaea163c103bf4f3a970
SyedSajjadHaider <sajjads26@gmail.com>
2022-04-24 19:38:49 +0530

    Learning list

---
+++ Intro_to_list.py
@@ -0,0 +1,20 @@
+# Array in python
+# Note: Python does not have built-in support for Arrays, but Python Lists can be used instead.
+
+# create a list 
+mylist = []
+
+#create a list with elements
+mylist2 = ["apple","orange","banana"]
+
+# traverse a list
+for element in mylist2:
+    print(element)
+
+mylist2.append("3.14")
+mylist2.append(False)
+mylist2.remove("apple")
+print(len(mylist2))
+
+# print a list
+print(mylist2)
